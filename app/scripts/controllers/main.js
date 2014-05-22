448d3498cfc870018a3854f9d3510de4104f316b
egandiol <edouard.gandiol@epfedu.fr>
2014-05-22 14:52:51 +0200

    create files

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,13 @@
 'use strict';
 
 angular.module('hotspotApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('MainCtrl', function ($scope, $http) {
+	$scope.names = [];
+
+	var getQ = function() {$http.get('http://public.opendatasoft.com/api/records/1.0/search?dataset=popular-baby-names-in-the-us&facet=column_1&facet=column_2&facet=column_3&facet=column_4').success( function (data) {
+		$scope.names=data;
+	});}
+
+
+	getQ();
+});
